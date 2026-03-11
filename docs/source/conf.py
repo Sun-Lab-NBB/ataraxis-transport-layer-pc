ecf0dfa6cea7be4a1431e4f34938a07695ec5872
Inkaros <ikondratv@gmail.com>
2026-03-11 10:32:37 -0400

    Upgraded to ataraxis-automation 8.1.0, time 6, and base-utilities 6.
      -- Updated pyproject.toml to PEP 639 license format and PEP 735 dependency groups.
      -- Modernized tox.ini with dependency_groups, hyphenated CLI flags, and template-compliant descriptions.
      -- Replaced sphinx-rtd-theme with furo in documentation configuration.
      -- Standardized project description to bare form across all canonical locations.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ import importlib_metadata
 # -- Project information -----------------------------------------------------
 project = 'ataraxis-transport-layer-pc'
 # noinspection PyShadowingBuiltins
-copyright = '2025, Sun (NeuroAI) lab'
+copyright = '2026, Sun (NeuroAI) lab'
 authors = ['Ivan Kondratyev', 'Katlynn Ryu']
 release = importlib_metadata.version("ataraxis-transport-layer-pc")
 
@@ -13,9 +13,7 @@ extensions = [
     'sphinx.ext.autodoc',        # To build documentation from python source code docstrings.
     'sphinx.ext.napoleon',       # To read google-style docstrings (works with autodoc module).
     'sphinx_autodoc_typehints',  # To parse typehints into documentation
-    'sphinx_rtd_theme',          # To format the documentation html using ReadTheDocs format.
     'sphinx_click',              # To read docstrings and command-line arguments from click-wrapped python functions.
-    'sphinx_rtd_dark_mode'       # Enables dark mode for RTD theme.
 ]
 
 templates_path = ['_templates']
@@ -45,8 +43,5 @@ typehints_formatter = None
 typehints_use_signature = False
 typehints_use_signature_return = False
 
-# Disables the dark mode by default.
-default_dark_mode = False
-
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'  # Directs sphinx to use RTD theme
+html_theme = 'furo'
